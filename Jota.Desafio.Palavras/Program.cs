8dd285e6292f4fcf51bd7bdcc12ab9586064544c
Leonardo Dorathoto <leonardo.dorathoto@gmail.com>
2024-09-05 09:05:05 -0300

    fix program

--- Jota.Desafio.Palavras/Program.cs
+++ Jota.Desafio.Palavras/Program.cs
@@ -1,30 +1,69 @@
-﻿namespace Jota.Desafio.Palavras;
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+using System.Data;
+using static Jota.Desafio.Palavras.Program;
+
+namespace Jota.Desafio.Palavras;
 internal class Program
 {
+    public class DataSet
+    {
+        [VectorType(4)] // Suporta até 4 letras por palavra
+        public float[] Letras { get; set; }
+        public float Pontuacao { get; set; }
+    }
+
+    public class WordScorePrediction
+    {
+        [ColumnName("Score")]
+        public float Score { get; set; }
+    }
+
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Digite uma série de palavras separadas por espaço:");
-        string frase = Console.ReadLine();
+        var mlContext = new MLContext();
 
-        string[] palavras = frase.Split(' ');
-        string palavraMaiorPontuacao = "";
-        int maiorPontuacao = 0;
-
-        foreach (string palavra in palavras)
+        var data = new[]
         {
-            int pontuacaoAtual = 0;
-            foreach (char letra in palavra)
-            {
-                pontuacaoAtual += letra - 'a' + 1;
-            }
+                new DataSet { Letras = new float[] { 1, 2, 1, 4 }, Pontuacao = 8f },  // "abad"
+                new DataSet { Letras = new float[] { 3, 1, 15 }, Pontuacao = 18f },   // "cao"
+                new DataSet { Letras = new float[] { 21, 22, 1 }, Pontuacao = 44f },  // "uva"
+                new DataSet { Letras = new float[] { 1 }, Pontuacao = 1f },           // "a"
+                new DataSet { Letras = new float[] { 1 }, Pontuacao = 1f },           // "A"
+                new DataSet { Letras = new float[] { 18, 15, 21, 16, 1 }, Pontuacao = 71f },  // "roupa"
+                new DataSet { Letras = new float[] { 18, 15, 5, 21 }, Pontuacao = 59f },  // "roeu"
+                new DataSet { Letras = new float[] { 18, 1, 20, 15 }, Pontuacao = 54f },  // "rato"
+                new DataSet { Letras = new float[] { 18, 15, 13, 1 }, Pontuacao = 47f },  // "roma"
+                new DataSet { Letras = new float[] { 18, 5, 9 }, Pontuacao = 32f },   // "rei"
+            };
 
-            if (pontuacaoAtual > maiorPontuacao)
-            {
-                maiorPontuacao = pontuacaoAtual;
-                palavraMaiorPontuacao = palavra;
-            }
+        var trainData = mlContext.Data.LoadFromEnumerable(data);
+
+        var pipeline = mlContext.Transforms.Concatenate("Features", "Letras")
+            .Append(mlContext.Regression.Trainers.Sdca(labelColumnName: "Pontuacao", maximumNumberOfIterations: 100));
+
+        // Treinar o modelo
+        var model = pipeline.Fit(trainData);
+
+        // Criar um mecanismo de previsão
+        var predictionEngine = mlContext.Model.CreatePredictionEngine<DataSet, WordScorePrediction>(model);
+
+        // Prever a pontuação de uma nova palavra
+        var newWord = new DataSet { Letras = new[] { 9f, 12f, 1f, 7f, 15f } }; // "idade"
+        var prediction = predictionEngine.Predict(newWord);
+        Console.WriteLine($"Pontuação prevista para 'idade': {prediction.Score}");
+
+        // Ordenar e exibir palavras com suas pontuações
+        var wordScores = data.ToDictionary(d => string.Concat(d.Letras.Select(l => (char)('a' + (int)l - 1))), d => d.Pontuacao);
+        var sortedWordScores = wordScores.OrderByDescending(pair => pair.Value);
+
+        int rank = 1;
+        foreach (var pair in sortedWordScores)
+        {
+            Console.WriteLine($"{rank}. {pair.Key}: {pair.Value}");
+            rank++;
         }
-        Console.WriteLine("Palavra de maior pontuação: " + palavraMaiorPontuacao);
-        Console.WriteLine("Pontuação: " + maiorPontuacao);
+
     }
 }
