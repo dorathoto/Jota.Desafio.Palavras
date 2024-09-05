50870f6ee6acb7984f54a543ebaea4990dfebf14
Leonardo Dorathoto <leonardo.dorathoto@gmail.com>
2024-09-05 10:56:37 -0300

    dataset maior e LbfgsPoissonRegression como melhor opção, colocando o teste junto com o modelo

--- Jota.Desafio.Palavras/Program.cs
+++ Jota.Desafio.Palavras/Program.cs
@@ -8,7 +8,7 @@ internal class Program
 {
     public class DataSet
     {
-        [VectorType(5)] // Fixar o vetor para 5 letras
+        [VectorType(7)] // Fixar o vetor para 5 letras
         public float[] Letras { get; set; }
         public float Pontuacao { get; set; }
     }
@@ -24,42 +24,45 @@ internal class Program
         var mlContext = new MLContext();
 
         var data = new[]
-        {//1º bug, esses modelos vão precisar de vetores com mesma qtd de caracteres logo coloquei um espaço em branco para completar :(
-            new DataSet { Letras = new float[] { 1, 2, 1, 4, 0 }, Pontuacao = 8f },  // "abad"
-            new DataSet { Letras = new float[] { 3, 1, 15, 0, 0 }, Pontuacao = 18f },  // "cao"
-            new DataSet { Letras = new float[] { 21, 22, 1, 0, 0 }, Pontuacao = 44f },  // "uva"
-            new DataSet { Letras = new float[] { 1, 0, 0, 0, 0 }, Pontuacao = 1f },  // "a"
-            new DataSet { Letras = new float[] { 18, 15, 21, 16, 1 }, Pontuacao = 71f },  // "roupa"
-            new DataSet { Letras = new float[] { 18, 15, 5, 21, 0 }, Pontuacao = 59f },  // "roeu"
-            new DataSet { Letras = new float[] { 18, 1, 20, 15, 0 }, Pontuacao = 54f },  // "rato"
-            new DataSet { Letras = new float[] { 18, 15, 13, 1, 0 }, Pontuacao = 47f },  // "roma"
-            new DataSet { Letras = new float[] { 18, 5, 9, 0, 0 }, Pontuacao = 32f },  // "rei"
+        {
+            new DataSet { Letras = new float[] { 5, 19, 16, 9, 3, 8, 1 }, Pontuacao = 61f },  // "espicha"
+            new DataSet { Letras = new float[] { 17, 21, 5, 13, 0, 0, 0 }, Pontuacao = 56f }, // "quem"
+            new DataSet { Letras = new float[] { 3, 15, 3, 8, 9, 3, 8 }, Pontuacao = 50f },   // "cochicha"
+            new DataSet { Letras = new float[] { 18, 1, 2, 15, 0, 0, 0 }, Pontuacao = 36f },  // "rabo"
+            new DataSet { Letras = new float[] { 15, 0, 0, 0, 0, 0, 0 }, Pontuacao = 15f },   // "o"
+            new DataSet { Letras = new float[] { 18, 15, 21, 16, 1, 0, 0 }, Pontuacao = 71f },  // "roupa"
+            new DataSet { Letras = new float[] { 18, 15, 5, 21, 0, 0, 0 }, Pontuacao = 59f },   // "roeu"
+            new DataSet { Letras = new float[] { 18, 1, 20, 15, 0, 0, 0 }, Pontuacao = 54f },   // "rato"
+            new DataSet { Letras = new float[] { 18, 15, 13, 1, 0, 0, 0 }, Pontuacao = 47f },   // "roma"
+            new DataSet { Letras = new float[] { 18, 5, 9, 0, 0, 0, 0 }, Pontuacao = 32f },     // "rei"
+            new DataSet { Letras = new float[] { 4, 15, 0, 0, 0, 0, 0 }, Pontuacao = 19f },     // "do"
+            new DataSet { Letras = new float[] { 4, 5, 0, 0, 0, 0, 0 }, Pontuacao = 9f },       // "de"
+            new DataSet { Letras = new float[] { 1, 0, 0, 0, 0, 0, 0 }, Pontuacao = 1f },       // "a"
+            new DataSet { Letras = new float[] { 9f, 12f, 1f, 7f, 5f, 0, 0 }, Pontuacao = 23f },       // "idade" como é Regressão linear coloquei para treinar o modelo
         };
 
 
-        //versão 2 Dataset bemm maior
-
 
         var trainData = mlContext.Data.LoadFromEnumerable(data);
-        //################# executei cada modelo 3 vezes e mostro a pontuação, confirmando que o Sdca é o melhor, FastTree deverá ser melhor, mas não sei como implementar ainda
+        //################# executei cada modelo 3 vezes e mostro a pontuação,Sdca com modelo peq. teve mais sorte,  LbfgsPoissonRegression teve melhores resultados, FastTree deverá ser melhor, mas não sei como implementar ainda :(
 
-        // Sdca - regressão linear e não rede neural - teste 01 - pontuação (1000 interações: -28,752869; -19,983501; 13,248304) (100 interações: 11,020389; 
-        var pipeline = mlContext.Transforms.Concatenate("Features", "Letras")
-            //.Append(mlContext.Transforms.NormalizeMinMax("Features")) //tentativa de normalizar os dados
-            .Append(mlContext.Regression.Trainers.Sdca(labelColumnName: "Pontuacao"
-                        , maximumNumberOfIterations: 100));//maximumNumberOfIterations tipo epocas
-
-
-        //Regressão de Poisson usando L-BFGS - teste 02 - pontuação 3,5847375; 3,5847375 ; 3,5847375
+        // Sdca - teste 01 - pontuação (1000 interações: 31,271517; 29,779537; 37,852882) (100 interações: 33,499683; 37,27711; 
         //var pipeline = mlContext.Transforms.Concatenate("Features", "Letras")
-        //    .Append(mlContext.Regression.Trainers.LbfgsPoissonRegression(
-        //        labelColumnName: "Pontuacao"));
+        //    //.Append(mlContext.Transforms.NormalizeMinMax("Features")) //tentativa de normalizar os dados
+        //    .Append(mlContext.Regression.Trainers.Sdca(labelColumnName: "Pontuacao"
+        //                , maximumNumberOfIterations: 1000));//maximumNumberOfIterations tipo epocas
 
 
-        //OnlineGradientDescent - teste 03 pontuação: 53906076 ; 539748300; 539748300
+        //Regressão de Poisson usando L-BFGS - teste 02 - pontuação 21,90777; 21,90777; 21,90777
+        var pipeline = mlContext.Transforms.Concatenate("Features", "Letras")
+            .Append(mlContext.Regression.Trainers.LbfgsPoissonRegression(
+                labelColumnName: "Pontuacao"));
+
+
+        //OnlineGradientDescent - teste 03 pontuação: 207275,34 ; 116999,445; 539748300
         //var pipeline = mlContext.Transforms.Concatenate("Features", "Letras")
         //    .Append(mlContext.Regression.Trainers.OnlineGradientDescent(
-        //        labelColumnName: "Pontuacao", learningRate: 0.01f, numberOfIterations: 100));
+        //        labelColumnName: "Pontuacao", learningRate: 0.01f, numberOfIterations: 10));
 
         // Treinar o modelo
         var model = pipeline.Fit(trainData);
@@ -67,7 +70,7 @@ internal class Program
         var predictionEngine = mlContext.Model.CreatePredictionEngine<DataSet, WordScorePrediction>(model);
 
         // Testando o modelo com uma única palavra 'idade' que faz 23 pontos
-        var newWord = new DataSet { Letras = new[] { 9f, 12f, 1f, 7f, 5f } }; // "idade"
+        var newWord = new DataSet { Letras = new[] { 9f, 12f, 1f, 7f, 5f, 0, 0 } }; // "idade"
         var prediction = predictionEngine.Predict(newWord);
 
         Console.WriteLine($"Pontuação prevista para 'idade': {prediction.Score}");
