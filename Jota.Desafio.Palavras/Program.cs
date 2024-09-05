f7dbc3e07a2acdc42987a07f1def1333c108b00f
Leonardo Dorathoto <leonardo.dorathoto@gmail.com>
2024-09-05 07:54:58 -0300

    primeira solução básica e eficaz

--- Jota.Desafio.Palavras/Program.cs
+++ Jota.Desafio.Palavras/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Digite uma série de palavras separadas por espaço:");
+            string frase = Console.ReadLine();
+
+            string[] palavras = frase.Split(' ');
+
+            string palavraMaiorPontuacao = "";
+            int maiorPontuacao = 0;
+
+            foreach (string palavra in palavras)
+            {
+                int pontuacaoAtual = 0;
+                foreach (char letra in palavra)
+                {
+                    pontuacaoAtual += letra - 'a' + 1;
+                }
+
+                if (pontuacaoAtual > maiorPontuacao)
+                {
+                    maiorPontuacao = pontuacaoAtual;
+                    palavraMaiorPontuacao = palavra;
+                }
+            }
+
+            Console.WriteLine("Palavra de maior pontuação: " + palavraMaiorPontuacao);
+            Console.WriteLine("Pontuação: " + maiorPontuacao);
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# Jota.Desafio.Palavras
+👨🏻‍💻 Bom, vamos lá, vou lançar meu desafio aqui, acredito que até (domingo 08-09-24) é uma boa data para entrega: 
+
+✅ Desafio Jotinha 🤷🏻
+
+Dada uma série de palavras, você precisa encontrar a palavra de maior pontuação.
+
+Cada letra de uma palavra pontua de acordo com sua posição no alfabeto: a = 1, b = 2, c = 3 etc.
+
+Por exemplo, a pontuação de abad É 8 (1 + 2 + 1 + 4).
+
+Você precisa retornar a palavra de maior pontuação como uma string.
+
+Se duas palavras marcam o mesmo, devolver a palavra que foi digitada primeiro.
+
+Todas as letras serão minúsculas e todas as entradas serão válidas.
+
+📅 Vamo que vamo, quero ver as tecnicas utilizadas, vou fazer a minha e depois discutimos as soluções.
+
+
+--------
+Fiz várias versões, cada uma em um branch diferente
+
+**master**
+Versão básica, eficaz utilizando um loop, já que cada letra é sequencial seus pontos, logo basta fazer o loop para descobrir a pontuação.
